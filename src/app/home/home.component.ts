6fe29cab3bd686c69e8675bfe656d59865e03310
realrgt <ergitorubildo@gmail.com>
2021-03-31 15:32:22 +0200

    copy hash

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -63,4 +63,18 @@ export class HomeComponent implements OnInit {
       padding: CryptoJS.pad.Pkcs7,
     }).toString(CryptoJS.enc.Utf8);
   }
+
+  copyText(val: string): void {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
 }
